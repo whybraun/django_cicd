1494dd26f67567fe26b73ab5ad1b001a791ba014
Dimitriy Murzin <dimitriymurzin@gmail.com>
2024-02-17 18:18:11 +0400

    add fix problems

--- logistic/test.py
+++ logistic/test.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
+
 from rest_framework.test import APIClient
 
 
-class TestSomething(TestCase):
+class MyTest(TestCase):
+    def test_ok(self):
+        assert True
+
     def test_sample_view(self):
+        url = "/api/v1/test/"
         client = APIClient()
-        response = client.get('/api/v1/test/')
-        self.assertEqual(response.status_code, 200)
+        response = client.get(url)
+        assert response.status_code == 200
--- logistic/views.py
+++ logistic/views.py
@@ -1,23 +1,27 @@
-from rest_framework.viewsets import ModelViewSet
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
-from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Stock, Product
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    pagination_class = LimitOffsetPagination
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [SearchFilter]
     search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
-    search_fields = ['products__title', 'products__description']
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Привет Мир!'})
